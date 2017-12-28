cc103a4dfa2778a04649a38eda1cabe0d8b3dc0e
Sachit Shrestha <ssachit3761@gmail.com>
2017-12-28 09:51:44 +0530

    Fetch Course Data from mockAPI

---
+++ src/actions/courseActions.js
@@ -0,0 +1,23 @@
+import * as types from './actionTypes';
+import courseApi from '../api/mockCourseApi';
+
+//Action creator
+export function loadCoursesSuccess(courses) {
+  return { 
+    type: types.LOAD_COURSES_SUCCESS, 
+    courses
+  };
+}
+
+
+
+//Thunk
+export function loadCourses() {
+  return function (dispatch) {
+    return courseApi.getAllCourses().then(courses => {
+      dispatch(loadCoursesSuccess(courses));
+    }).catch(error => {
+      throw(error);
+    });
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,20 @@
+import "babel-polyfill";
+import React from "react";
+import { render } from "react-dom";
+import { Provider } from 'react-redux';
+import configureStore from './store/configureStore';
+import { Router, browserHistory } from 'react-router';
+
+import routes from './routes';
+import "./styles/styles.css";
+import { loadCourses } from './actions/courseActions';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+
+const store = configureStore();
+store.dispatch(loadCourses());
+render(
+  <Provider store={store}>
+  <Router history={browserHistory} routes={routes} />
+  </Provider>,
+  document.getElementById("app")
+);
