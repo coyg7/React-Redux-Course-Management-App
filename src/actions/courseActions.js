42237ab6cd6e9b430f7b905df7d4bc651c8e6851
coyg7 <ssachit3761@gmail.com>
2017-12-29 10:09:57 +0530

    Add error handler for CourseForm

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -1,6 +1,6 @@
 import * as types from './actionTypes';
 import courseApi from '../api/mockCourseApi';
-import {beginAjaxCall} from './ajaxStatusActions';
+import {beginAjaxCall, ajaxCallError} from './ajaxStatusActions';
 
 //Action creator
 export function loadCoursesSuccess(courses) {
@@ -44,6 +44,7 @@ export function saveCourse(course) {
     return courseApi.saveCourse(course).then(course => {
       course.id ? dispatch(updateCourseSuccess(course)) : dispatch(createCourseSuccess(course));
     }).catch(error => {
+      dispatch(ajaxCallError(error));
       throw(error);
     });
   };
